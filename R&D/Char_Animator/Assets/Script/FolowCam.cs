6dc829aca88d9b667a0414da1edc75f9ecec034a
CJH <tmqrlrns0109@gmail.com>
2020-07-07 18:12:01 +0900

    <2020-07-07> 1. FollowCam.cs - 카메라 추적 구현 완료 - Tag : Player 를 검색해서 해당 Object에 붙도록 설정
    2. Weapon Prefab
    - Transform 수정해서 재 프리팹화
    
    3. Animation
    - Shoot Animation 실행 시 사격 에임이 맞지 않던 부분 수정 : Character GameObject의 localRotation.y 값을 0에서 40으로 수정

---
+++ R&D/Char_Animator/Assets/Script/FolowCam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolowCam : MonoBehaviour
+{
+    //추적할 대상
+    public Transform target;
+    //카메라와의 거리   
+    public float dist = 7f;
+    //카메라의 높이 
+    public float height = 5f;
+
+
+    private Transform tr;
+
+    void Start()
+    {
+        // 시작과 동시에 Tag : Player를 찾아서 Target 설정 후 Trace
+        this.target = FindObjectOfType<Player>().transform;
+        tr = GetComponent<Transform>();
+    }
+ 
+    private void LateUpdate()
+    {
+        //카메라 위치 설정
+        tr.position = target.position - (1 * Vector3.forward * dist) + (Vector3.up * height);
+        tr.LookAt(target);
+    }
+
+}
--- R&D/Char_Animator/Assets/Script/Player.cs
+++ R&D/Char_Animator/Assets/Script/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -12,8 +13,6 @@ public class Player : MonoBehaviour
 
 
     // Test Code
-    [SerializeField]
-    private bool Joystick_test = false;
     [SerializeField]
     private Transform target;
 
@@ -37,9 +36,27 @@ public class Player : MonoBehaviour
     {
         anim = model.GetComponent<Animator>();
 
+        // Collider Test==========
+        Collider[] collisions = Physics.OverlapSphere(this.transform.position, 5f);
+        foreach(Collider coll in collisions)
+        {
+            if(coll.gameObject.tag == "Enemy")
+            {
+                Debug.LogFormat($"Enermy GameObject Name : {coll.gameObject.name}");
+
+                this.anim.SetBool("Shoot", true);
+                this.target = coll.transform;
+                break;
+            }
+        }
+        //=========================
+        
+
         if (this.anim.GetBool("Shoot") == true)
         {
             this.transform.LookAt(target);
+            
+            this.model.transform.localRotation = Quaternion.Euler(0, 40, 0);        // 사격 애니메이션 초점 맞추기 위해
             if (joystick.Vertical != 0 || joystick.Horizontal != 0)
             {
                 this.anim.Play("Run");
@@ -50,6 +67,8 @@ public class Player : MonoBehaviour
         }
         else if (this.anim.GetBool("Shoot") == false)
         {
+            this.model.transform.localRotation = Quaternion.Euler(0, 0, 0);        // 사격 애니메이션 끝난 후 다시 원상복구 시키기 위해
+
             if (joystick.Vertical != 0 || joystick.Horizontal != 0)
             {
                 this.anim.Play("Run");
@@ -62,52 +81,7 @@ public class Player : MonoBehaviour
         if (this.anim.GetBool("Shoot"))
             this.anim.Play("Shoot");
     }
-    //void Update()
-    //{
-    //    anim = model.GetComponent<Animator>();
-
-    //    if (Joystick_test == true)
-    //    {
-    //        this.transform.LookAt(target);
-    //        if (joystick.Vertical != 0 || joystick.Horizontal != 0)
-    //        {
-    //            this.anim.Play("Run");
-    //            this.transform.Translate(new Vector3(joystick.Direction.x, 0, joystick.Direction.y) * this.speed * Time.deltaTime, Space.World);
-    //        }
-    //        else
-    //            this.anim.Play("Idle");
-    //    }
-    //    else if (Joystick_test == false)
-    //    {
-    //        if (joystick.Vertical != 0 || joystick.Horizontal != 0)
-    //        {
-    //            this.anim.Play("Run");
-    //            this.transform.Translate(Vector3.forward * this.speed * Time.deltaTime);
-    //            this.transform.rotation = Quaternion.Euler(0f, Mathf.Atan2(joystick.Horizontal, joystick.Vertical) * Mathf.Rad2Deg, 0f);
-    //        }
-    //        else
-    //            this.anim.Play("Idle");
-    //    }
-
-    //    //if (this.anim.GetBool("Shoot"))
-    //    //    this.anim.Play("Shoot");
-
-    //    Debug.LogFormat($" GetBool : {this.anim.GetBool("Shoot")}");
-    //    //if (Input.GetKeyUp(KeyCode.Space))
-    //    //{
-    //    //    this.anim.SetBool("Shoot", false);
-    //    //    Debug.Log("Space Click Up!!!");
-    //    //}
-    //    //else if (Input.GetKeyDown(KeyCode.Space))
-    //    //{
-    //    //    this.anim.SetBool("Shoot", true);
-    //    //    Debug.Log("Space Click Down!!!");
-    //    //}
-    //    //if (this.anim.GetBool("Shoot"))
-    //    //    this.transform.LookAt(target);
-    //    //else
-    //    //    this.target = null;
-    //}
+    
     private void SetPlayerState(ePlayerState state)
     {
         eState = state;
@@ -119,6 +93,10 @@ public class Player : MonoBehaviour
         {
             this.anim.SetBool("Shoot", true);
             this.target = other.transform;
+        }
+        else if(other.tag == "Item")
+        {
+            other.gameObject.SetActive(false);
         }
     }
     private void OnTriggerExit(Collider other)
